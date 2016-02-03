e84c687da7cb3e618c4d6cba262138ab6f47d890
Francesco Longo <flongo@unime.it>
2016-02-03 16:45:53 +0100

    Completed some TODOs for student REST APIs

--- TODO.md
+++ TODO.md
@@ -2,20 +2,22 @@
 
 ## REST APIs
 
+ * implement paging in lists
+ * store passwords in an encripted way
  * put the configuration file in a global position
- * choose what fields to provide in the general list and what field to provide in the details for each resource (class, student, environment)
- * decide if delete operations should return an error if the element to delete does not exist
- * decide if details operations should return an error if the element does not exist
- * in each update call where one or more field are updated return an error if the resource to update does not exist
+ * choose what fields to provide in the general list and what field to provide in the details for each resource (class, ~~student~~, environment)
+ * decide if delete operations should return an error if the element to delete does not exist (class, ~~student~~, environment)
+ * decide if details operations should return an error if the element does not exist (class, ~~student~~, environment)
+ * in each update call where one or more field are updated return an error if the resource to update does not exist (class, ~~student~~, environment)
  * in the "list of all students in a class" operation return an error if the class does not exist
  * in the "add a student in a class" and "DELETE delete a student from a class" operations return an error if the class does not exist
- * check the error codes (404, 500, and so on) for all the REST API calls
+ * check the error codes (404, 500, and so on) for all the REST API calls (class, ~~student~~, environment)
  * in the "list of all the environments for a student" operation return an error if the student does not exist
  * in the "create an environments for a student" and "delete an environments for a student" return an error if the student does not exist
  * in the "start/stop an environment" return an error if the environment does not exist
  * when a student is deleted, she should be removed from her classes and her environments should be removed
  * implement authentication for all the operations (the teacher should be able to perform all the operations while students should be able to perform operations only on their environments)
- * check and correct the return types for all the REST APIs
+ * check and correct the return types for all the REST APIs (class, ~~student~~, environment)
  * insert information about types in the README file
  * ~~fix portfinder: put a wrapper that controls that port already assigned to stopped containers are not picked up again when creating new containers~~
  
--- routes/students.js
+++ routes/students.js
@@ -9,43 +9,79 @@
 var express = require('express');
 var router = express.Router();
 
-//list all students, create a student, remove a student, update an attribute (name, surname, age, gender, username, password
-
 // GET list of all students
 router.get('/', function(req, res, next) {
     var db = req.db;
-    var collection = db.get('students');
-    collection.find({},{},function(err,docs){
-        res.json(docs);
+    var students = db.get('students');
+    students.find({},{fields : {name: 1, surname: 1, username: 1}},function(err,docs){
+        if(err){
+            res.json({error: err});
+        }
+        else{
+            res.json(docs);
+        }
     });
 });
 
 // GET details of a student
-router.get('/:id/details', function(req, res, next) {
+router.get('/:id/', function(req, res, next) {
     var db = req.db;
-    var collection = db.get('students');
+    var students = db.get('students');
     var studentToShow = req.params.id;
-    collection.find({ '_id': studentToShow },{},function(err,docs){
-        res.json(docs);
+    students.findOne({ '_id': studentToShow },{fields: {password: 0}},function(err,docs){
+        if(err){
+            res.json({error: err});
+        }
+        else if(docs == null){
+            res.status(404).json({ error: "Student with id '" + studentToShow + "' does not exist."});
+        }else{            
+            res.json(docs);
+
+        }
     });
 });
 
 //POST add a student
 router.post('/', function(req, res) {
     var db = req.db;
-    var collection = db.get('students');
-    collection.insert(req.body, function(err, docs){
-        res.json((err === null) ? { '_id': docs._id } : { msg: err });
-    });
+    var students = db.get('students');
+    if(req.body.name == undefined || req.body.surname == undefined || req.body.username == undefined || req.body.password == undefined){
+        res.status(400).json({error: "Missing parameters: name, surname, username, and password are mandatory."});
+    }
+    else{
+        students.findOne({username: req.body.username},{},function(err,docs){
+            if(err){
+                res.json({error: err});
+            }
+            else if(docs == null){
+                students.insert(req.body, function(err, docs){
+                    res.json((err === null) ? docs : { error: err });
+                });
+            }
+            else{
+                res.status(400).json({error: "Student with username " + req.body.username + " already exists."});
+            }
+        });
+    }
 });
 
 //DELETE delete a student
 router.delete('/:id', function(req, res) {
     var db = req.db;
-    var collection = db.get('students');
+    var students = db.get('students');
     var studentToDelete = req.params.id;
-    collection.remove({ '_id' : studentToDelete }, function(err,docs) {
-        res.json((err === null) ? {} : { msg:'error: ' + err });
+    students.findOne({_id: studentToDelete},{}, function(err, docs){
+        if(err){
+            res.json({error: err});
+        }
+        else if(docs == null){
+            res.status(404).json({ error: "Student with id '" + studentToDelete + "' does not exist."});
+        }
+        else{
+            students.remove({ '_id' : studentToDelete }, function(err,docs) {
+                res.json((err === null) ? {} : { error: err });
+            });
+        }
     });
 });
 
@@ -53,12 +89,24 @@ router.delete('/:id', function(req, res) {
 router.put('/:id', function(req, res) {
     var extend = require('util')._extend
     var db = req.db;
-    var collection = db.get('students');
+    var students = db.get('students');
     var studentToUpdate = req.params.id;
-    var output = extend({},{ '_id' : studentToUpdate });
-    extend(output,  req.body);
-    collection.update({ '_id' : studentToUpdate }, req.body, function(err) {
-        res.json((err === null) ? output  : { msg:'error: ' + err });
+    students.findOne({ _id: studentToUpdate },{}, function(err, docs){
+        if(err){
+            res.json(err);
+        }
+        else if(docs == null){
+            res.status(404).json({ error: "Student with id '" + studentToUpdate + "' does not exist."});
+        }
+        else{
+            students.update({ '_id' : studentToUpdate }, req.body, function(err) {
+                
+                var newStudent = extend({},{ '_id' : studentToUpdate });
+                extend(newStudent,  req.body);
+                
+                res.json((err === null) ? newStudent : { error: err });
+            });
+        }
     });
 });
 
